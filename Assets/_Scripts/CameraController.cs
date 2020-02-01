14c4d4862bccff9db49fea16ce2b3cb007a6f070
Bash Kurgunayev <sebastiankurg@gmail.com>
2020-02-01 17:09:13 -0500

    Fixed character movement with camera follow.

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool lockCursor;
+    public float sensitivity = 10;
+    public Transform target, player;
+
+    float pitch;
+    float yaw;
+
+    public float rotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    public Vector3 currentRotation;
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        pitch -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, -40, 85);
+
+        transform.LookAt(target);
+        target.rotation = Quaternion.Euler(pitch, yaw, 0);
+        player.rotation = Quaternion.Euler(0, yaw, 0);
+    }
+}
