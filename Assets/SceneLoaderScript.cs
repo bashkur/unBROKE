7e086628c90c58f75a014172f4519c12dd194c8a
Elijah Maloy <47044742+Mallboy@users.noreply.github.com>
2020-02-01 13:45:49 -0500

    Character Selection scene is made

---
+++ Assets/SceneLoaderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    private bool isLeft;
+    private bool isRight;
+    public void leftReady()
+    {
+        isLeft = true;
+        if(isRight)
+        {
+            bothReady();
+        }
+    }
+
+    public void rightReady()
+    {
+        isRight = true;
+        if(isLeft)
+        {
+            bothReady();
+        }
+    }
+
+    private void bothReady()
+    {
+
+    }
+}
