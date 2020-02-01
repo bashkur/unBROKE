451422169ee2f48ba3df2f3407406f94cfb6de2e
Elijah Maloy <47044742+Mallboy@users.noreply.github.com>
2020-02-01 14:40:38 -0500

    Added the player spawner onto the main scene. The spawner looks into the character lists child and pulls out the selected character and takes it out, isolating it. Then the spawner deletes himself.

---
+++ Assets/_Scripts/PlayerSpawnerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnerScript : MonoBehaviour
+{
+    private Transform playerTransform;
+    public int player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = this.transform.GetChild(0).GetChild(PlayerPrefs.GetInt("P"+player+"CharacterSelected"));
+
+        playerTransform.parent = null;
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
