f2e18f66edb47ffed374157e716b147929bf0a81
Bash Kurgunayev <sebastiankurg@gmail.com>
2020-02-02 16:52:31 -0500

    Added HUD.

--- Assets/_Scripts/HUD.cs
+++ Assets/_Scripts/HUD.cs
@@ -7,18 +7,21 @@ public class HUD : MonoBehaviour
 {
     public GameObject gameMgr;
     public GameObject canvas;
+    Text timeText;
+    TimerScript timerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("HUD");
-
+        timerScript = gameMgr.GetComponent<TimerScript>();
+        timeText = canvas.transform.GetChild(2).GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeText.text = "" + timerScript.minLeft() + ":" + timerScript.secLeft();
     }
 
     public void UpdateDamage(float ratio)
