69a91fcee8f8ce2c9a7f794c0ae6dfb060342f00
LordLud <nludowese@yahoo.com>
2020-02-02 13:39:26 -0500

    will randomly break things

---
+++ Assets/autoBreak.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoBreak : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] B_Objects = GameObject.FindGameObjectsWithTag("Breakable");
+        for(int i = 0; i<3; i++)
+        {
+            int itterator = Random.Range(0, B_Objects.Length);
+            BreakableObjectScript breakable= B_Objects[itterator].GetComponent<BreakableObjectScript>();
+            breakable.damage();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
