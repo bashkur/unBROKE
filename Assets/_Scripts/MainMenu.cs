2d0656e0344dcc332fd7e19237f4a53c468dd1d4
LordLud <nludowese@yahoo.com>
2020-02-02 16:44:42 -0500

    TEXT FOR WIN

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -9,7 +9,16 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       int thing = PlayerPrefs.GetInt("DID I WIN?");
+       if (thing==1)
+        {
+            this.transform.GetChild(4).GetComponent<Text>().enabled = true;
+
+        }
+       else
+        {
+            this.transform.GetChild(4).GetComponent<Text>().enabled = false;
+        }
     }
 
     // Update is called once per frame
