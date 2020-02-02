1edbdba7b3231c2c1da562bd81637725baafdd01
ChaseKerr <chasekerr18@yahoo.com>
2020-02-01 19:43:25 -0500

    Added and applied materials to Boys_House prefab

---
+++ Assets/_Scripts/NpcController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcController : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] rooms;
+    Transform destination;
+
+    bool isSearching = true;
+    bool isHunting = false;
+    bool isBreaking = false;
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+
+        if (agent != null)
+        {
+            SetDestination();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDestination(Transform dest = null)
+    {
+        // go to given position
+        if (dest != null)
+        {
+            destination = dest;
+            agent.SetDestination(destination.position);
+        }
+        else
+        {
+            // choose random room to navigate to
+            destination = rooms[Random.Range(0, rooms.Length - 1)];
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // interactable item in range
+        if (other.tag == "Breakable")
+        {
+            // check if item is not yet broken
+            /*
+            if (!broken && isSearching) // FIXME
+            {
+                // break item
+            }
+            */
+        }
+    }
+}
